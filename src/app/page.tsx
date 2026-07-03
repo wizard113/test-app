4c177938fe1c2c46c76a034aa5c20d3c71c52fd4
wizard113 <yeong.rok.lee@gmail.com>
2026-07-03 22:51:46 +0900

    37th

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,8 @@ import Image from 'next/image';
 import styles from './page.module.css';
 
 export default function Home() {
+  const secret = process.env.JWT_SECRET;
+  const DB_URL = process.env.DATABASE_URL;
   return (
     <div className={styles.page}>
       <main className={styles.main}>
@@ -15,50 +17,9 @@ export default function Home() {
         />
         <div className={styles.intro}>
           <h1>Next github action 배포22</h1>
-          <p>
-            Looking for a starting point or more instructions? Head over to{' '}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Templates
-            </a>{' '}
-            or the{' '}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learning
-            </a>{' '}
-            center.
-          </p>
-        </div>
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className={styles.secondary}
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+
+          <h1>secret: {secret}</h1>
+          <h1>dburl: {DB_URL}</h1>
         </div>
       </main>
     </div>
